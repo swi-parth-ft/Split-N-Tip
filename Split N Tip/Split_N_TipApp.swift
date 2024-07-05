271512dec525530a001ecb4dda71aa40686212fb
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-05 09:51:29 -0400

    Initial Commit

---
+++ Split N Tip/Split_N_TipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_N_TipApp.swift
+//  Split N Tip
+//
+//  Created by Parth Antala on 2024-07-05.
+//
+
+import SwiftUI
+
+@main
+struct Split_N_TipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
