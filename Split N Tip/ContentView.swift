e0de13f21778dffe3c251004ff16d80d9b5b96fc
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-05 17:46:58 -0400

    finalize Design

--- Split N Tip/ContentView.swift
+++ Split N Tip/ContentView.swift
@@ -9,32 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+    }
     
   
     
     var body: some View {
+        NavigationStack {
             VStack {
                 formView()
-               // scrollView()
+                // scrollView()
+                
             }
             .background(TimelineView(.animation) { timeline in
                 let x = (sin(timeline.date.timeIntervalSince1970) + 1) / 2
-
+                
                 MeshGradient(width: 3, height: 3, points: [
                     [0, 0], [0.5, 0], [1, 0],
-                    [0, 0.5], [Float(x), Float(x)], [1, 0.5],
+                    [0, 0.5], [Float(x), 0.5], [1, 0.5],
                     [0, 1], [0.5, 1], [1, 1]
                 ], colors: [
-                    .blue, .blue, .orange,
-                    .red, .orange, .yellow,
+                    .black, .blue, .orange,
+                    .black, .orange, .black,
                     .blue, .red, .orange
                 ])
             }
                 .ignoresSafeArea()
-                
+                        
             )
             
         }
+    }
         
     }
     
@@ -42,7 +48,7 @@ struct ContentView: View {
 
 struct formView: View {
     
-    @State private var checkAmount: Double = 0.0
+    @State private var checkAmount: Double = 220.0
     @State private var numberOfPeople: Int = 2
     @State private var tipPercentage: Int = 20
     @FocusState private var amountIsFocuced: Bool
@@ -59,35 +65,29 @@ struct formView: View {
     
     let tipPercentages: [Int] = [10, 15, 20, 25, 0]
     
+    @State private var animate: Bool = false
     var body: some View {
-        ZStack {
-            Color.white.opacity(0.6)
-                            .blur(radius: 88)
-                            .edgesIgnoringSafeArea(.all)
+        VStack {
             Form {
                 
-                
-                
-                Section {
+                Section(header: Text("How much is the bill?").foregroundStyle(.white)) {
                     
                     TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocuced)
                         
-                    
-                    
                     Picker("Number of People", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
+                        ForEach(2..<10) {
                             Text("\($0) People")
                         }
                     }
-                    .pickerStyle(.navigationLink)
+                    .pickerStyle(.menu)
                     
                 }
                 .listRowBackground(Color.white.opacity(0.3))
                 
-                Section {
-                    Text("How much tip do you want to leave?")
+                Section(header: Text("How much tip do you want to leave?").foregroundStyle(.white)) {
+                    
                     Picker("Tip Percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
                             Text($0, format: .percent)
@@ -96,21 +96,57 @@ struct formView: View {
                     .pickerStyle(.segmented)
                 }
                 .listRowBackground(Color.white.opacity(0.3))
-                
-                Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
-                    .listRowBackground(Color.white.opacity(0.3))
+
                     
             }
+            .frame(height: UIScreen.main.bounds.width - 130)
             .scrollContentBackground(.hidden)
-           
-            
+            .navigationTitle("SplitNTip")
+        }
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.white)
+                    .opacity(0)
+                    .shadow(radius: 10)
+                    .frame(width: 350)
+                    .padding()
+                VStack {
+                    Text("Y'll owe").fontWidth(.expanded)
+                    Spacer()
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
+                        .font(.system(size: 90 , design: .rounded))
+                        .foregroundStyle(
+                            MeshGradient(width: 3, height: 3, points: [
+                                [0, 0], [0.5, 0], [1, 0],
+                                [0, 0.5], [0.5, 0.5], [1, 0.5],
+                                [0, 1], [0.5, 1], [1, 1]
+                            ], colors: [
+                                .purple, .yellow, .green,
+                                .yellow, .green, .yellow,
+                                .green, .yellow, .green
+                            ])
+                        )
+                        .rotationEffect(.degrees(animate ? -2 : 2 ))
+                        .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true), value: animate)
+                        .onAppear {
+                            animate.toggle()
+                        }
+                    Spacer()
+                    Text("each").fontWidth(.expanded)
+                }
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            }
         }
         
         
-        
+       
         
     }
 }
+
+
 
 struct scrollView: View {
     let animals: [String] = ["animal1", "animal2", "animal3"]
